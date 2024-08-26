7a56d1ba95b6a29740fc27cfa03c10dce6f44fcc
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2024-08-26 13:55:44 -0700

    feat: initial version (#1)

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-# 🚧 Work in progress - see [#1](https://github.com/copilot-extensions/preview-sdk.js/pull/1)
-
 # `@copilot-extensions/preview-sdk`
+
+> SDK for building GitHub Copilot Extensions
+
+⚠️ **This SDK is a preview and subjetct to change**. We will however adhere to [semantic versioning](https://semver.org/), so it's save to use for early experimentation. Just beware there will be breaking changes. Best to watch this repository's releases for updates.
+
+## Usage
+
+### `verify(rawBody, signature, keyId, options)`
+
+```js
+import { verify } from "@copilot-extensions/preview-sdk";
+
+const payloadIsVerified = await verify(request.body, signature, keyId, {
+  token,
+});
+// true or false
+```
 
 ## Contributing
 
---
+++ test/integration.test.js
@@ -0,0 +1,120 @@
+import { test } from "node:test";
+import assert from "node:assert/strict";
+
+import { request as defaultRequest } from "@octokit/request";
+import { MockAgent } from "undici";
+
+import { verify } from "../index.js";
+
+const RAW_BODY = `{"copilot_thread_id":"9a1cc23a-ab73-498b-87a5-96c94cb7e3f3","messages":[{"role":"user","content":"@gr2m hi","copilot_references":[{"type":"github.repository","data":{"type":"repository","id":102985470,"name":"sandbox","ownerLogin":"gr2m","ownerType":"User","readmePath":"README.md","description":"@gr2m's little sandbox to play","commitOID":"9b04fffccbb818b2e317394463731b66f1ec5e89","ref":"refs/heads/main","refInfo":{"name":"main","type":"branch"},"visibility":"public","languages":[{"name":"JavaScript","percent":100}]},"id":"gr2m/sandbox","is_implicit":false,"metadata":{"display_name":"gr2m/sandbox","display_icon":"","display_url":""}}],"copilot_confirmations":null},{"role":"user","content":"@gr2m test","copilot_references":[{"type":"github.repository","data":{"type":"repository","id":102985470,"name":"sandbox","ownerLogin":"gr2m","ownerType":"User","readmePath":"README.md","description":"@gr2m's little sandbox to play","commitOID":"9b04fffccbb818b2e317394463731b66f1ec5e89","ref":"refs/heads/main","refInfo":{"name":"main","type":"branch"},"visibility":"public","languages":[{"name":"JavaScript","percent":100}]},"id":"gr2m/sandbox","is_implicit":false,"metadata":{"display_name":"gr2m/sandbox","display_icon":"","display_url":""}}],"copilot_confirmations":null},{"role":"user","content":"@gr2m test","copilot_references":[{"type":"github.repository","data":{"type":"repository","id":102985470,"name":"sandbox","ownerLogin":"gr2m","ownerType":"User","readmePath":"README.md","description":"@gr2m's little sandbox to play","commitOID":"9b04fffccbb818b2e317394463731b66f1ec5e89","ref":"refs/heads/main","refInfo":{"name":"main","type":"branch"},"visibility":"public","languages":[{"name":"JavaScript","percent":100}]},"id":"gr2m/sandbox","is_implicit":false,"metadata":{"display_name":"gr2m/sandbox","display_icon":"","display_url":""}}],"copilot_confirmations":null},{"role":"user","content":"Current Date and Time (UTC): 2024-08-26 19:43:13\\nUser's Current URL: https://github.com/gr2m/sandbox\\nCurrent User's Login: gr2m\\n","name":"_session","copilot_references":[],"copilot_confirmations":null},{"role":"user","content":"","copilot_references":[{"type":"github.repository","data":{"type":"repository","id":102985470,"name":"sandbox","ownerLogin":"gr2m","ownerType":"User","readmePath":"README.md","description":"@gr2m's little sandbox to play","commitOID":"9b04fffccbb818b2e317394463731b66f1ec5e89","ref":"refs/heads/main","refInfo":{"name":"main","type":"branch"},"visibility":"public","languages":[{"name":"JavaScript","percent":100}]},"id":"gr2m/sandbox","is_implicit":false,"metadata":{"display_name":"gr2m/sandbox","display_icon":"","display_url":""}}],"copilot_confirmations":null},{"role":"user","content":"test","copilot_references":[],"copilot_confirmations":[]}],"stop":null,"top_p":0,"temperature":0,"max_tokens":0,"presence_penalty":0,"frequency_penalty":0,"copilot_skills":null,"agent":"gr2m"}`;
+const KEY_ID =
+  "4fe6b016179b74078ade7581abf4e84fb398c6fae4fb973972235b84fcd70ca3";
+
+const CURRENT_PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAELPuPiLVQbHY/clvpNnY+0BzYIXgo
+S0+XhEkTWUZEEznIVpS3rQseDTG6//gEWr4j9fY35+dGOxwOx3Z9mK3i7w==
+-----END PUBLIC KEY-----
+`;
+
+const SIGNATURE =
+  "MEYCIQC8aEmkYA/4EQrXEOi2OL9nfpbnrCxkMc6HrH7b6SogKgIhAIYBThcpzkCCswiV1+pOaPI+zFQF9ShG61puoKs9rJjq";
+
+test("smoke", (t) => {
+  assert.equal(typeof verify, "function");
+});
+
+test("minimal usage", async (t) => {
+  const mockAgent = new MockAgent();
+  function fetchMock(url, opts) {
+    opts ||= {};
+    opts.dispatcher = mockAgent;
+    return fetch(url, opts);
+  }
+
+  mockAgent.disableNetConnect();
+  const mockPool = mockAgent.get("https://api.github.com");
+  mockPool
+    .intercept({
+      method: "get",
+      path: `/meta/public_keys/copilot_api`,
+    })
+    .reply(
+      200,
+      {
+        public_keys: [
+          {
+            key: CURRENT_PUBLIC_KEY,
+            key_identifier: KEY_ID,
+            is_current: true,
+          },
+        ],
+      },
+      {
+        headers: {
+          "content-type": "application/json",
+          "x-request-id": "<request-id>",
+        },
+      },
+    );
+  const testRequest = defaultRequest.defaults({
+    request: { fetch: fetchMock },
+  });
+
+  const result = await verify(RAW_BODY, SIGNATURE, KEY_ID, {
+    request: testRequest,
+  });
+
+  assert.deepEqual(result, true);
+});
+
+test("invalid arguments", (t) => {
+  assert.rejects(verify(RAW_BODY, SIGNATURE), {
+    name: "Error",
+    message: "[@copilot-extensions/preview-sdk] Invalid keyId",
+  });
+
+  assert.rejects(verify("", SIGNATURE, KEY_ID), {
+    name: "Error",
+    message: "[@copilot-extensions/preview-sdk] Invalid payload",
+  });
+
+  assert.rejects(verify(1, SIGNATURE, KEY_ID), {
+    name: "Error",
+    message: "[@copilot-extensions/preview-sdk] Invalid payload",
+  });
+
+  assert.rejects(verify(undefined, SIGNATURE, KEY_ID), {
+    name: "Error",
+    message: "[@copilot-extensions/preview-sdk] Invalid payload",
+  });
+
+  assert.rejects(verify(RAW_BODY, "", KEY_ID), {
+    name: "Error",
+    message: "[@copilot-extensions/preview-sdk] Invalid signature",
+  });
+
+  assert.rejects(verify(RAW_BODY, 1, KEY_ID), {
+    name: "Error",
+    message: "[@copilot-extensions/preview-sdk] Invalid signature",
+  });
+
+  assert.rejects(verify(RAW_BODY, undefined, KEY_ID), {
+    name: "Error",
+    message: "[@copilot-extensions/preview-sdk] Invalid signature",
+  });
+
+  assert.rejects(verify(RAW_BODY, SIGNATURE, ""), {
+    name: "Error",
+    message: "[@copilot-extensions/preview-sdk] Invalid keyId",
+  });
+
+  assert.rejects(verify(RAW_BODY, SIGNATURE, 1), {
+    name: "Error",
+    message: "[@copilot-extensions/preview-sdk] Invalid keyId",
+  });
+
+  assert.rejects(verify(RAW_BODY, SIGNATURE, undefined), {
+    name: "Error",
+    message: "[@copilot-extensions/preview-sdk] Invalid keyId",
+  });
+});
