bd517acf5c5c0dcff4943774c6097a7b13e644d6
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2024-09-02 15:38:26 -0700

    test: `options.tools` for `prompt()`

--- test/prompt.test.js
+++ test/prompt.test.js
@@ -33,7 +33,7 @@ test("minimal usage", async (t) => {
             content: "What is the capital of France?",
           },
         ],
-        model: "gpt-4o-mini",
+        model: "gpt-4",
       }),
     })
     .reply(
@@ -57,7 +57,82 @@ test("minimal usage", async (t) => {
 
   const result = await prompt("What is the capital of France?", {
     token: "secret",
-    model: "gpt-4o-mini",
+    model: "gpt-4",
+    request: { fetch: fetchMock },
+  });
+
+  t.assert.deepEqual(result, {
+    requestId: "<request-id>",
+    message: {
+      content: "<response text>",
+    },
+  });
+});
+
+test("function calling", async (t) => {
+  const mockAgent = new MockAgent();
+  function fetchMock(url, opts) {
+    opts ||= {};
+    opts.dispatcher = mockAgent;
+    return fetch(url, opts);
+  }
+
+  mockAgent.disableNetConnect();
+  const mockPool = mockAgent.get("https://api.githubcopilot.com");
+  mockPool
+    .intercept({
+      method: "post",
+      path: `/chat/completions`,
+      body: JSON.stringify({
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are a helpful assistant. Use the supplied tools to assist the user.",
+          },
+          { role: "user", content: "Call the function" },
+        ],
+        model: "gpt-4",
+        toolChoice: "auto",
+        tools: [
+          {
+            type: "function",
+            function: { name: "the_function", description: "The function" },
+          },
+        ],
+      }),
+    })
+    .reply(
+      200,
+      {
+        choices: [
+          {
+            message: {
+              content: "<response text>",
+            },
+          },
+        ],
+      },
+      {
+        headers: {
+          "content-type": "application/json",
+          "x-request-id": "<request-id>",
+        },
+      }
+    );
+
+  const result = await prompt("Call the function", {
+    token: "secret",
+    model: "gpt-4",
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "the_function",
+          description: "The function",
+        },
+      },
+    ],
     request: { fetch: fetchMock },
   });
 
