71d617691c8222ce1786d02366a92d96064004b4
francisfuzz <15894826+francisfuzz@users.noreply.github.com>
2024-09-11 13:35:34 +0000

    refactor: use ava in parse.test.js

--- test/parse.test.js
+++ test/parse.test.js
@@ -1,4 +1,4 @@
-import { test, suite } from "node:test";
+import test from 'ava';
 
 import { MockAgent } from "undici";
 import { request as defaultRequest } from "@octokit/request";
@@ -17,126 +17,125 @@ import {
   SIGNATURE,
 } from "./verification.test.js";
 
-suite("request parsing", () => {
-  test("parseRequestBody()", (t) => {
-    // parseRequestBody() does not check for structure. We assume it adheres
-    // to the expected structure when we verify that request came indeed
-    // from GitHub
-    const payload = parseRequestBody('{"messages": []}');
-    t.assert.deepStrictEqual(payload.messages, []);
-  });
 
-  test("transformPayloadForOpenAICompatibility()", (t) => {
-    const payload = transformPayloadForOpenAICompatibility({
-      messages: [
-        {
-          role: "role",
-          name: "name",
-          content: "content",
-          someCopilotKey: "value",
-        },
-      ],
-      someCopilotKey: "value",
-    });
-    t.assert.deepStrictEqual(payload.messages, [
+test("parseRequestBody()", (t) => {
+  // parseRequestBody() does not check for structure. We assume it adheres
+  // to the expected structure when we verify that request came indeed
+  // from GitHub
+  const payload = parseRequestBody('{"messages": []}');
+  t.deepEqual(payload.messages, []);
+});
+
+test("transformPayloadForOpenAICompatibility()", (t) => {
+  const payload = transformPayloadForOpenAICompatibility({
+    messages: [
       {
         role: "role",
         name: "name",
         content: "content",
+        someCopilotKey: "value",
       },
-    ]);
+    ],
+    someCopilotKey: "value",
   });
+  t.deepEqual(payload.messages, [
+    {
+      role: "role",
+      name: "name",
+      content: "content",
+    },
+  ]);
+});
 
-  test("verifyAndParseRequest()", async (t) => {
-    const mockAgent = new MockAgent();
-    function fetchMock(url, opts) {
-      opts ||= {};
-      opts.dispatcher = mockAgent;
-      return fetch(url, opts);
-    }
+test("verifyAndParseRequest()", async (t) => {
+  const mockAgent = new MockAgent();
+  function fetchMock(url, opts) {
+    opts ||= {};
+    opts.dispatcher = mockAgent;
+    return fetch(url, opts);
+  }
 
-    mockAgent.disableNetConnect();
-    const mockPool = mockAgent.get("https://api.github.com");
-    mockPool
-      .intercept({
-        method: "get",
-        path: `/meta/public_keys/copilot_api`,
-      })
-      .reply(
-        200,
-        {
-          public_keys: [
-            {
-              key: CURRENT_PUBLIC_KEY,
-              key_identifier: KEY_ID,
-              is_current: true,
-            },
-          ],
-        },
-        {
-          headers: {
-            "content-type": "application/json",
-            "x-request-id": "<request-id>",
-          },
-        },
-      );
-    const testRequest = defaultRequest.defaults({
-      request: { fetch: fetchMock },
-    });
-
-    const result = await verifyAndParseRequest(RAW_BODY, SIGNATURE, KEY_ID, {
-      request: testRequest,
-    });
-
-    t.assert.deepStrictEqual(
-      { isValidRequest: true, payload: JSON.parse(RAW_BODY) },
-      result,
-    );
-  });
-
-  test("getUserMessage()", (t) => {
-    const payload = {
-      messages: [
-        {
-          content: "Some previous message",
-        },
-        {
-          content: "Hello, world!",
-        },
-      ],
-    };
-    const result = getUserMessage(payload);
-    t.assert.equal("Hello, world!", result);
-  });
-
-  test("getUserConfirmation()", (t) => {
-    const payload = {
-      messages: [
-        {
-          content: "Some previous message",
-        },
-        {
-          content: "Hello, world!",
-          copilot_confirmations: [
-            {
-              state: "accepted",
-              confirmation: {
-                id: "some-confirmation-id",
-                someConfirmationMetadata: "value",
-              },
-            },
-          ],
-        },
-      ],
-    };
-    const result = getUserConfirmation(payload);
-    t.assert.deepStrictEqual(
+  mockAgent.disableNetConnect();
+  const mockPool = mockAgent.get("https://api.github.com");
+  mockPool
+    .intercept({
+      method: "get",
+      path: `/meta/public_keys/copilot_api`,
+    })
+    .reply(
+      200,
       {
-        accepted: true,
-        id: "some-confirmation-id",
-        metadata: { someConfirmationMetadata: "value" },
+        public_keys: [
+          {
+            key: CURRENT_PUBLIC_KEY,
+            key_identifier: KEY_ID,
+            is_current: true,
+          },
+        ],
       },
-      result,
+      {
+        headers: {
+          "content-type": "application/json",
+          "x-request-id": "<request-id>",
+        },
+      },
     );
+  const testRequest = defaultRequest.defaults({
+    request: { fetch: fetchMock },
   });
+
+  const result = await verifyAndParseRequest(RAW_BODY, SIGNATURE, KEY_ID, {
+    request: testRequest,
+  });
+
+  t.deepEqual(
+    { isValidRequest: true, payload: JSON.parse(RAW_BODY) },
+    result,
+  );
+});
+
+test("getUserMessage()", (t) => {
+  const payload = {
+    messages: [
+      {
+        content: "Some previous message",
+      },
+      {
+        content: "Hello, world!",
+      },
+    ],
+  };
+  const result = getUserMessage(payload);
+  t.is("Hello, world!", result);
+});
+
+test("getUserConfirmation()", (t) => {
+  const payload = {
+    messages: [
+      {
+        content: "Some previous message",
+      },
+      {
+        content: "Hello, world!",
+        copilot_confirmations: [
+          {
+            state: "accepted",
+            confirmation: {
+              id: "some-confirmation-id",
+              someConfirmationMetadata: "value",
+            },
+          },
+        ],
+      },
+    ],
+  };
+  const result = getUserConfirmation(payload);
+  t.deepEqual(
+    {
+      accepted: true,
+      id: "some-confirmation-id",
+      metadata: { someConfirmationMetadata: "value" },
+    },
+    result,
+  );
 });
