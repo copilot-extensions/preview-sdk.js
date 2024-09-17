f61a49dada31fb682929860474d6859b38f25abb
Rob Bos <rajbos@users.noreply.github.com>
2024-09-17 18:10:12 +0200

    docs(examples): hello world, prompt (#84)
    See the conversation in #83 
    
    - Adding `examples` folder with easy to start projects (see `README` in
    that folder)
    - Example 1: hello world
    - Example 2: handle the user prompt by sending it to the Copilot API and
    returning the result
    
    ---------
    
    Co-authored-by: Gregor Martynus <39992+gr2m@users.noreply.github.com>

---
+++ examples/hello-world/index.js
@@ -0,0 +1,22 @@
+import { createServer } from "node:http";
+
+import {
+  createTextEvent,
+  createDoneEvent,
+} from "@copilot-extensions/preview-sdk";
+
+const server = createServer((request, ressponse) => {
+  console.log(`Received [${request.method}] to [${request.url}]`);
+
+  if (request.method === "GET") {
+    return ressponse.end("ok");
+  }
+
+  ressponse.write(createTextEvent("Hello, world!"));
+  ressponse.end(createDoneEvent());
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
